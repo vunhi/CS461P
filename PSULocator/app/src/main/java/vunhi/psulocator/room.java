bf5876f5700487aba4cb02e88558956f1d940d34
Nhi Vu <vunhi@pdx.edu>
2017-07-13 19:46:31 -0700

    updated readme + beginning to start spinner for rooms

--- PSULocator/app/src/main/java/vunhi/psulocator/room.java
+++ PSULocator/app/src/main/java/vunhi/psulocator/room.java
@@ -1,24 +1,17 @@
 package vunhi.psulocator;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.Intent;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-public class building extends AppCompatActivity {
-
-    private ImageView buildingImage;
-    private Spinner chooseRoom;
-    int [] buildings = {R.array.cramer};
-    private boolean isFirstSelection = true;
+public class room extends AppCompatActivity {
+    ImageView chosenRoom;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,12 +20,12 @@ public class building extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    Intent goToHome = new Intent(building.this, MainActivity.class);
+                    Intent goToHome = new Intent(room.this, MainActivity.class);
                     startActivity(goToHome);
                     return true;
 
                 case R.id.navigation_search:
-                    Intent goToSearch = new Intent(building.this, MainActivity.class);
+                    Intent goToSearch = new Intent(room.this, MainActivity.class);
                     startActivity(goToSearch);
                     MainActivity.index = 1;
                     return true;
@@ -46,13 +39,15 @@ public class building extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building);
-        Intent goToBuilding = getIntent();
+        Intent goToRoom = getIntent();
 
-        //Setting floor plan image
-        String building = goToBuilding.getStringExtra("building");
-        int res = getResources().getIdentifier(building, "drawable", this.getPackageName());
-        buildingImage = (ImageView) findViewById(R.id.buildingImage);
-        buildingImage.setImageResource(res);
+        //Setting chosen room highlighted image
+        String room = goToRoom.getStringExtra("room");
+        room.replaceAll("\\s+", "");
+        room = room.toLowerCase();
+        int res = getResources().getIdentifier(room, "drawable", this.getPackageName());
+        chosenRoom = (ImageView) findViewById(R.id.chosenRoom);
+        chosenRoom.setImageResource(res);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Right now I have successfully implemented the interactive map aspect of the the 
 
 Now I must incorporate the option of choosing different floors and different rooms for each building. 
 
-For the remainder of the term that is what I will be working on as well as adding the search functionality and improving the ease of use of the app as well as the general app theme. If I have time I hope to accomplish this for every floor and room number for each building (though that is quite a lot of buildings!)
+For the remainder of the term that is what I will be working on as well as adding the search functionality and improving the ease of use of the app as well as the general app theme. If I have time I hope to accomplish this for every floor and room number for each building (though that is quite a lot of buildings!). I also need to finish filling out the project ReadMe; as you can see, it's pretty bare right now. 
 
 Some difficulties I'm facing: figuring out how to incorporate the interactive navigation bar for each page. Also, I find that working with the android xml files is very difficult. Using the design function is not as intuitive as it seems: dragging widgets across the screen is not entirely smooth and it can throw off the layout of all other widgets on the screen. And even though the layout appears one way on the projected screen in AS it is soemtimes not the same as what appears on the actual phone screen. This is a very stressful obstacle to overcome. 
 
